76f944a0250564ad16150d642c6e2267198a3421
Mariette Olwagen <90972322+MarietteO@users.noreply.github.com>
2023-08-07 10:08:44 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Here we go FizzBuzz! 3...2...1...")
+print(" ")
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz!")
+    elif num % 3 == 0:
+        print("Fizz!")
+    elif num % 5 == 0:
+        print("Buzz!")
+    else:
+        print(num)
